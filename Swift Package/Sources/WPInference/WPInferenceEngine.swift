f6e641068f1035104887f357d758adfc1cdfbe13
Alex Hoppen <alex@alexhoppen.de>
2020-04-15 16:14:09 +0200

    Teach the WPInferenceEngine to infer through JumpInstructions

--- Swift Package/Sources/WPInference/WPInferenceEngine.swift
+++ Swift Package/Sources/WPInference/WPInferenceEngine.swift
@@ -1,8 +1,11 @@
 import IR
 
 fileprivate struct WPInferenceState {
-  var position: InstructionPosition?
+  /// The position up to which WP-inference has run.
+  /// This means that the instruction at this position **has already been inferred**.
+  var position: InstructionPosition
   
+  /// The term that has been inferred so far up to this program position.
   var term: WPTerm
 }
 
@@ -13,68 +16,110 @@ public class WPInferenceEngine {
     self.program = program
   }
   
-  private func preceedingInstructionPosition(of position: InstructionPosition) -> InstructionPosition? {
-    if position.instructionIndex > 0 {
-      return InstructionPosition(basicBlock: position.basicBlock, instructionIndex: position.instructionIndex - 1)
+  private func branchesToInfer(before state: WPInferenceState) -> [(position: InstructionPosition, term: WPTerm)] {
+    if state.position.instructionIndex > 0 {
+      let previousPosition = InstructionPosition(basicBlock: state.position.basicBlock, instructionIndex: state.position.instructionIndex - 1)
+      return [(position: previousPosition, term: state.term)]
     } else {
-      let predecessorBlocks = program.directPredecessors[position.basicBlock]!
+      let predecessorBlocks = program.directPredecessors[state.position.basicBlock]!
       if predecessorBlocks.isEmpty {
-        return nil
-      } else {
-        assert(predecessorBlocks.count <= 1, "Multi-block predecessors not implemented yet")
-        let predecessorBlock = predecessorBlocks.first!
-        let instructionIndex = program.basicBlocks[predecessorBlock]!.instructions.count - 1
-        return InstructionPosition(basicBlock: predecessorBlock, instructionIndex: instructionIndex)
+        fatalError("Already reached the start of the program. Nothing to infer before it.")
       }
+      var branches: [(position: InstructionPosition, term: WPTerm)] = []
+      for predecessor in predecessorBlocks {
+        let branchPosition = InstructionPosition(
+          basicBlock: predecessor,
+          instructionIndex: program.basicBlocks[predecessor]!.instructions.count - 1
+        )
+        let instruction = program.instruction(at: branchPosition)
+        switch instruction {
+        case is JumpInstruction:
+          // The jump jumps unconditionally, so there is no need to modify the state
+          branches.append((branchPosition, state.term))
+        case is BranchInstruction:
+          fatalError("Not implemented")
+        default:
+          fatalError("Block that jumps to a different block should have terminated with a jump or branch instruction")
+        }
+      }
+      return branches
     }
   }
   
   public func infer(term: WPTerm) -> Double {
-    var inferenceState = WPInferenceState(position: program.returnPosition, term: term)
+    let programStartState = InstructionPosition(basicBlock: program.startBlock, instructionIndex: 0)
     
-    while let position = inferenceState.position {
-      switch program.instruction(at: position) {
-      case is ReturnInstruction:
-        inferenceState.position = preceedingInstructionPosition(of: position)
-      case let instruction as AssignInstruction:
-        inferenceState.term = inferenceState.term.replacing(variable: instruction.assignee, with: WPTerm(instruction.value))
-        inferenceState.position = preceedingInstructionPosition(of: position)
-      case let instruction as AddInstruction:
-        inferenceState.term = inferenceState.term.replacing(
-          variable: instruction.assignee,
-          with: WPTerm(instruction.lhs) + WPTerm(instruction.rhs)
-        )
-        inferenceState.position = preceedingInstructionPosition(of: position)
-      case let instruction as SubtractInstruction:
-        inferenceState.term = inferenceState.term.replacing(
-          variable: instruction.assignee,
-          with: WPTerm(instruction.lhs) - WPTerm(instruction.rhs)
-        )
-        inferenceState.position = preceedingInstructionPosition(of: position)
-      case let instruction as CompareInstruction:
-        fatalError("not implemented: \(instruction)")
-      case let instruction as DiscreteDistributionInstruction:
-        var terms: [WPTerm] = []
-        for (value, probability) in instruction.distribution {
-          let term = .double(probability) * inferenceState.term.replacing(variable: instruction.assignee, with: .integer(value))
-          terms.append(term)
+    var inferenceStatesWorklist: [WPInferenceState] = [WPInferenceState(position: program.returnPosition, term: term)]
+    var finishedInferenceStates: [WPInferenceState] = []
+    
+    while let worklistEntry = inferenceStatesWorklist.popLast() {
+      for (position, term) in self.branchesToInfer(before: worklistEntry) {
+        let newStateToInfer: WPInferenceState
+        switch program.instruction(at: position) {
+        case let instruction as AssignInstruction:
+          let newTerm = term.replacing(
+            variable: instruction.assignee,
+            with: WPTerm(instruction.value)
+          )
+          newStateToInfer = WPInferenceState(
+            position: position,
+            term: newTerm
+          )
+        case let instruction as AddInstruction:
+          let newTerm = term.replacing(
+            variable: instruction.assignee,
+            with: WPTerm(instruction.lhs) + WPTerm(instruction.rhs)
+          )
+          newStateToInfer = WPInferenceState(
+            position: position,
+            term: newTerm
+          )
+        case let instruction as SubtractInstruction:
+          let newTerm = term.replacing(
+            variable: instruction.assignee,
+            with: WPTerm(instruction.lhs) - WPTerm(instruction.rhs)
+          )
+          newStateToInfer = WPInferenceState(
+            position: position,
+            term: newTerm
+          )
+        case let instruction as CompareInstruction:
+          fatalError("not implemented: \(instruction)")
+        case let instruction as DiscreteDistributionInstruction:
+          var terms: [WPTerm] = []
+          for (value, probability) in instruction.distribution {
+            let term = .double(probability) * term.replacing(variable: instruction.assignee, with: .integer(value))
+            terms.append(term)
+          }
+          newStateToInfer = WPInferenceState(
+            position: position,
+            term: .add(terms: terms)
+          )
+        case let instruction as ObserveInstruction:
+          fatalError("not implemented: \(instruction)")
+        case is JumpInstruction:
+          // Already handled by branchesToInfer. Nothing to do anymore.
+          newStateToInfer = WPInferenceState(position: position, term: term)
+        case let instruction as BranchInstruction:
+          fatalError("not implemented: \(instruction)")
+        case is ReturnInstruction:
+          fatalError("WP inference is initialised at the ReturnInstruction which means the ReturnInstruction has already been inferred")
+        case let instruction as PhiInstruction:
+          fatalError("not implemented: \(instruction)")
+        case let unknownInstruction:
+          fatalError("Unknown instruction: \(type(of: unknownInstruction))")
         }
-        inferenceState.term = .add(terms: terms)
-        inferenceState.position = preceedingInstructionPosition(of: position)
-      case let instruction as ObserveInstruction:
-        fatalError("not implemented: \(instruction)")
-      case let instruction as JumpInstruction:
-        fatalError("not implemented: \(instruction)")
-      case let instruction as BranchInstruction:
-        fatalError("not implemented: \(instruction)")
-      case let instruction as PhiInstruction:
-        fatalError("not implemented: \(instruction)")
-      default:
-        fatalError("Unknown instruction: \(type(of: program.instruction(at: position)))")
+        if newStateToInfer.position == programStartState {
+          finishedInferenceStates.append(newStateToInfer)
+        } else {
+          inferenceStatesWorklist.append(newStateToInfer)
+        }
       }
     }
     
-    switch inferenceState.term.simplified {
+    let combinedTerm = WPTerm.add(terms: finishedInferenceStates.map(\.term))
+    
+    switch combinedTerm.simplified {
     case .integer(let value):
       return Double(value)
     case .double(let value):
--- Swift Package/Tests/WPInferenceTests/WPInferenceEngineTests.swift
+++ Swift Package/Tests/WPInferenceTests/WPInferenceEngineTests.swift
@@ -50,4 +50,76 @@ class WPInferenceEngineTests: XCTestCase {
     XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(3))), 0.4)
     XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(4))), 0)
   }
+
+  func testInferProgramWithJump() {
+    let bb0Name = BasicBlockName("bb0")
+    let bb1Name = BasicBlockName("bb1")
+
+    let var0 = IRVariable(name: "0", type: .int)
+    let var1 = IRVariable(name: "1", type: .int)
+
+    let bb0 = BasicBlock(name: bb0Name, instructions: [
+      DiscreteDistributionInstruction(assignee: var0, distribution: [1: 0.6, 2: 0.4]),
+      JumpInstruction(target: bb1Name),
+    ])
+
+    let bb1 = BasicBlock(name: bb1Name, instructions: [
+      AddInstruction(assignee: var1, lhs: .variable(var0), rhs: .integer(1)),
+      ReturnInstruction(),
+    ])
+
+    let irProgram = IRProgram(startBlock: bb0Name, basicBlocks: [bb0, bb1])
+    // bb0:
+    //   int %0 = int 1
+    //   jump bb1
+    //
+    // bb1:
+    //   int %1 = add int %0 int 1
+    //   return
+
+    let inferenceEngine = WPInferenceEngine(program: irProgram)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(2))), 0.6)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(3))), 0.4)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(4))), 0)
+  }
+  
+  func testInferProgramWithTwoSuccessiveJump() {
+    let bb0Name = BasicBlockName("bb0")
+    let bb1Name = BasicBlockName("bb1")
+    let bb2Name = BasicBlockName("bb2")
+
+    let var0 = IRVariable(name: "0", type: .int)
+    let var1 = IRVariable(name: "1", type: .int)
+
+    let bb0 = BasicBlock(name: bb0Name, instructions: [
+      DiscreteDistributionInstruction(assignee: var0, distribution: [1: 0.6, 2: 0.4]),
+      JumpInstruction(target: bb1Name),
+    ])
+    
+    let bb1 = BasicBlock(name: bb1Name, instructions: [
+      JumpInstruction(target: bb2Name),
+    ])
+
+    let bb2 = BasicBlock(name: bb2Name, instructions: [
+      AddInstruction(assignee: var1, lhs: .variable(var0), rhs: .integer(1)),
+      ReturnInstruction(),
+    ])
+
+    let irProgram = IRProgram(startBlock: bb0Name, basicBlocks: [bb0, bb1, bb2])
+    // bb0:
+    //   int %0 = int 1
+    //   jump bb1
+    //
+    // bb1:
+    //   jump bb2
+    //
+    // bb2:
+    //   int %1 = add int %0 int 1
+    //   return
+
+    let inferenceEngine = WPInferenceEngine(program: irProgram)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(2))), 0.6)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(3))), 0.4)
+    XCTAssertEqual(inferenceEngine.infer(term: .equal(lhs: .variable(var1), rhs: .integer(4))), 0)
+  }
 }
